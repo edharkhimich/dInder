713cd9ce96afa670fc0b206c9906ad45feb4253f
Edgar Khimich <edgarkhimich@gmail.com>
2018-11-16 00:59:55 -0600

    lets start

---
+++ App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import { Provider } from 'mobx-react'
+import AppNavigator from './app/app.navigator'
+import stores from './app/stores'
+
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <Provider stores={stores}>
+        <AppNavigator />
+      </Provider>
+    )
+  }
+}
+
---
+++ app/stores/index.js
@@ -0,0 +1,5 @@
+import ConfigStore from './settings.stores'
+
+const config = new ConfigStore()
+
+export default { config }
